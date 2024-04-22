d4e209a3285d9860f0bf449ab2b3e4004381eec8
ardian <a.haxhiu@head.wtf>
2024-04-22 02:06:22 +0200

    transaction id added.

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\PaymentTransaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 use Illuminate\Support\Facades\Log;
@@ -10,6 +11,16 @@ class PaymentController extends Controller
 {
     public function initPayment()
     {
+        $lastTransaction = PaymentTransaction::orderBy('id', 'DESC')->first();
+
+        $date = \Carbon\Carbon::now();
+        $year = $date->year;
+        $month = $date->month;
+        $month = $month < 10 ? '0' . $month : $month;
+        $day = $date->day;
+        $day = $day < 10 ? '0' . $day : $day;
+        $transactionId = $year . '-' . $month . '-' . $day . '-' . ($lastTransaction ? $lastTransaction->id + 1 : 1);
+
         try {
             $response = Http::withHeaders([
                 'Content-Type' => 'application/json',
@@ -17,7 +28,7 @@ class PaymentController extends Controller
                 'Authorization' => 'Basic ' . base64_encode('press-api:G4P4bs)4+I_V2nHKdCv3u+?YiVe1G'),
                 'X-Signature' => 'OQxsFuuj4ifcLFaPAPyuO6TtaC65Yb'
             ])->post('https://asxgw.paymentsandbox.cloud/api/v3/transaction/press-simulator/debit', [
-                'merchantTransactionId' => '20200-04-11-07',
+                'merchantTransactionId' => $transactionId,
                 'amount' => '9.99',
                 'currency' => 'EUR',
                 'transactionToken' => request()->token,
@@ -25,6 +36,13 @@ class PaymentController extends Controller
                 'cancelUrl' => 'https://5e5e-46-99-63-213.ngrok-free.app/redirected.php',
                 'errorUrl' => 'https://5e5e-46-99-63-213.ngrok-free.app/redirected.php'
             ]);
+
+            PaymentTransaction::create([
+                'user_id' => 1,
+                'transaction_id' => $transactionId,
+                'data' => "{}"
+            ]);
+
             return $response->body();
         } catch (\Exception $e) {
             Log::info(json_encode($e->getMessage()));
