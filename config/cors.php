871ae6a21371002057ffce51ae35ccbd5e27d7db
ardian <a.haxhiu@head.wtf>
2024-04-23 02:05:27 +0200

    fix cors.

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['https://umrlice.vercel.app/*'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
