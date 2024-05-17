e12b5bf305c339ece0010580a3adb3f1cf004d2b
ardian <a.haxhiu@head.wtf>
2024-05-17 02:13:57 +0200

    fix.

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -39,7 +39,7 @@ class PaymentController extends Controller
                 'successUrl' => 'https://umrlice.vercel.app/payment-success',
                 'cancelUrl' => 'https://umrlice.vercel.app/payment-error',
                 'errorUrl' => 'https://umrlice.vercel.app/payment-error',
-                'callbackUrl' => 'https://www.umrlice-api.com/api/e6dfcdd0d0e693508e7f14ad1841cfe8/payment/callback'
+                'callbackUrl' => '/api/e6dfcdd0d0e693508e7f14ad1841cfe8/payment/callback'
             ]);
 
             $jsonResponse = $response->body();
