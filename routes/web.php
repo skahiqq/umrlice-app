ed0f25d5fd698fb7950616ecab4da2e1c6616a86
ardian <a.haxhiu@head.wtf>
2024-05-20 15:15:23 +0200

    testing.

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', function () {
 Route::get('/test', function () {
     $lastTransactionDetails = PaymentTransaction::where('user_id', 94)->orderBy('created_at', 'DESC')->first();
 
-    return $lastTransactionDetails->data['uuid'];
+    return json_decode($lastTransactionDetails->data)['uuid'];
     try {
         $response = Http::withHeaders([
             'Content-Type' => 'application/json',
