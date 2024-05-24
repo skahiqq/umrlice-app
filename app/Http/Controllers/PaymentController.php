2325c76dc5dcf89fd60a0da7591e66f58bd10ca1
ardian <a.haxhiu@head.wtf>
2024-05-24 03:34:41 +0200

    price and post id fix.

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -198,9 +198,9 @@ class PaymentController extends Controller
                 'user_id' => $request->user_id,
                 'post_id' => $lastTransaction->post_id,
                 'price' => $lastTransaction->price,
-                'transaction_id' => (json_decode($response->body(), TRUE)['transactionStatus'] == 'ERROR' ? PaymentTransaction::TYPE[4] : PaymentTransaction::TYPE[3]) . '_' . $transactionId,
+                'transaction_id' => ($concatResponseBody->transactionStatus == 'ERROR' ? PaymentTransaction::TYPE[4] : PaymentTransaction::TYPE[3]) . '_' . $transactionId,
                 'data' => $responseBody,
-                'type' => json_decode($response->body(), TRUE)['transactionStatus'] == 'ERROR' ? 4 : 3
+                'type' => $concatResponseBody->transactionStatus == 'ERROR' ? 4 : 3
             ]);
 
             if (!$lastTransaction->sent) {
