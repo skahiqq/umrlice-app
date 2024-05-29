314bdc112708d185e00f5ea58043ea9daea92a66
ardian <a.haxhiu@head.wtf>
2024-05-29 18:24:53 +0200

    testing.

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,18 @@ Route::get('/', function () {
 });
 
 Route::get('/test', function () {
+
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, 'https://parseapi.back4app.com/classes/Country?count=1&limit=0');
+    curl_setopt($curl, CURLOPT_HTTPHEADER, array(
+        'X-Parse-Application-Id: mxsebv4KoWIGkRntXwyzg6c6DhKWQuit8Ry9sHja', // This is the fake app's application id
+        'X-Parse-Master-Key: TpO0j3lG2PmEVMXlKYQACoOXKQrL3lwM0HwR9dbH' // This is the fake app's readonly master key
+    ));
+    $data = json_decode(curl_exec($curl)); // Here you have the data that you need
+    print_r(json_encode($data, JSON_PRETTY_PRINT));
+    curl_close($curl);
+
+    dd($data);
     $response = Http::withHeaders([
         'Content-Type' => 'application/json',
         'Accept' => 'application/json',
