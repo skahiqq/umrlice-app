adf682dd3df9cc5527c6b5a0439df88add4dda0e
ardian <a.haxhiu@head.wtf>
2024-07-31 00:45:12 +0200

    testing

--- app/Models/Spent.php
+++ app/Models/Spent.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -14,4 +15,13 @@ class Spent extends Model
         'description',
         'type'
     ];
+
+    protected $appends = [
+        'month'
+    ];
+
+    public function getMonthAttribute()
+    {
+        return Carbon::parse($this->attributes['created_at'])->monthName;
+    }
 }
