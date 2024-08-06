80bda80db3ceae413d290c917ed56915eb12fd01
ardian <a.haxhiu@head.wtf>
2024-08-06 13:55:52 +0200

    testing

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -155,7 +155,7 @@ class PaymentController extends Controller
             'post_id' => request()->post_id
         ])->first();
 
-        return $preAuthorizeTransaction;
+        return $preAuthorizeTransaction ?? null;
 
         try {
             $response = Http::withHeaders([
